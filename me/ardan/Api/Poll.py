306c3dd02701dc65281d1e2c4ac9a4446fb7d502
Ciel991 <35801688+Ciel991@users.noreply.github.com>
2018-02-15 20:28:43 +0700

    Update Poll.py

--- me/ardan/Api/Poll.py
+++ me/ardan/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = 'Line/8.0.1'
-  LA = 'WIN10 7.18.1  HELLO-WORLD 11.2.5'    
+  UA = 'Line/5.5.1.1.590'
+  LA = 'DESKTOPWIN\t5.5.1.1.1590\tWINDOWS_NT\t8.1'    
 
   rev = 0
 
